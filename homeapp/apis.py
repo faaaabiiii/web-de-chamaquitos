cd1342de6d8e96c9e8be43dac2f02b03056a2bbb
aztabuu <avergara@usm.cl>
2023-10-25 21:57:27 -0300

    http.client

--- homeapp/apis.py
+++ homeapp/apis.py
@@ -1,12 +1,18 @@
-from django import requests
+import http.client
 
-url = "https://climate-change-live452.p.rapidapi.com/news"
+conn = http.client.HTTPSConnection("climate-change-live452.p.rapidapi.com")
+
 headers = {
-	"X-RapidAPI-Key": "9acd896290mshd7266af432403dcp12ac41jsn0b84bdd47bfa",
-	"X-RapidAPI-Host": "climate-change-live452.p.rapidapi.com"
+    'X-RapidAPI-Key': "9acd896290mshd7266af432403dcp12ac41jsn0b84bdd47bfa",
+    'X-RapidAPI-Host': "climate-change-live452.p.rapidapi.com"
 }
-response = requests.get(url, headers=headers)
-news = response.json()
+
+conn.request("GET", "/news", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+news = data.decode("utf-8")
 
 ''' 
 Noticias de cambio climático en una lista de diccionarios con llaves 'title' 'url' 'source'
