2d6dac4588eb9bace880618a47bbbd360f9d48e2
aztabuu <avergara@usm.cl>
2023-11-08 19:39:51 -0300

    forms y models

--- homeapp/models.py
+++ homeapp/models.py
@@ -3,11 +3,10 @@ from django.db import models
 class Noticia2(models.Model):
     titulo = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=200)
-    url = models.CharField(max_length=300)
-    url_img = models.CharField(max_length=300)
+    url = models.URLField()
+    url_img = models.URLField()
     likes = models.IntegerField()
     shared = models.IntegerField()
-    links = models.URLField()
     
     def __str__(self):
         return self.title
