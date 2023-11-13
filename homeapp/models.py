2eab5fcc78505daeb46428a5251098ad7720b0ec
faaaabiiii <fabigonza.gc@gmail.com>
2023-11-13 13:45:05 -0300

    TIene error la parte de los modelos

--- homeapp/models.py
+++ homeapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Noticias(models.Model):
     titulo = models.CharField(max_length=50, blank=False)
@@ -7,7 +8,9 @@ class Noticias(models.Model):
     url_img = models.URLField(blank=False)
     likes = models.IntegerField(null=True, blank=True)
     shared = models.IntegerField(null=True, blank=True)
-    
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+
+
     def __str__(self):
         return self.titulo
 
